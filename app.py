2a3a9f51c004093b8080ed0707768343e2dee7a6
Sumit-Baisware-Esyasoft <sumit.baisware@esyasoft.com>
2025-10-29 13:48:56 +0530

    Refactor system information header to separate record count for improved readability

--- app.py
+++ app.py
@@ -182,7 +182,8 @@ hierarchy_df = load_hierarchy_data()
 
 # ----------------- SYSTEM INFORMATION SECTION -----------------
 st.markdown("<div class='custom-card'>", unsafe_allow_html=True)
-st.markdown(f"### 🗂️ सिस्टम जानकारी | System Information ({hierarchy_df['Msn'].shape[0]}) Records Found")
+st.markdown(f"### 🗂️ सिस्टम जानकारी | System Information")
+st.markdown(f"({hierarchy_df['Msn'].shape[0]}) Records Found")
 
 if hierarchy_df is not None:
     with st.expander("🔽 विवरण चुनें | Select Details", expanded=True):
