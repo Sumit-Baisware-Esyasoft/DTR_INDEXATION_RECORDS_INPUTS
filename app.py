f8a0a8f0e18243d13c64c6ff40db969f13373cfe
Sumit-Baisware-Esyasoft <sumit.baisware@esyasoft.com>
2025-11-17 15:31:05 +0530

    Update hierarchy file path to DTR Master Information 06_1 and adjust last update date

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ except Exception as e:
 @st.cache_data
 def load_hierarchy_data():
     try:
-        hierarchy_path = r"DTR Master Information_05.xlsx"
+        hierarchy_path = r"DTR Master Information_06_1.xlsx"
         return pd.read_excel(hierarchy_path)
     except Exception as e:
         st.error(f"Error loading master file: {e}")
@@ -183,7 +183,7 @@ hierarchy_df = load_hierarchy_data()
 # ----------------- SYSTEM INFORMATION SECTION -----------------
 st.markdown("<div class='custom-card'>", unsafe_allow_html=True)
 st.markdown(f"### 🗂️ सिस्टम जानकारी | System Information")
-st.markdown(f"Last Update 06/11/2025 12:00 PM ({hierarchy_df['Msn'].shape[0]}) Records Found")
+st.markdown(f"Last Update 17/11/2025 12:00 PM ({hierarchy_df['Msn'].shape[0]}) Records Found")
 
 if hierarchy_df is not None:
     with st.expander("🔽 विवरण चुनें | Select Details", expanded=True):
